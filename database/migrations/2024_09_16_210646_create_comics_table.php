c7a787480455a16cb2e3680953cd12561d56338f
Vinctdea <vincenzo.deangelis23@gmail.com>
2024-09-16 23:12:56 +0200

    feat: add table db

---
+++ database/migrations/2024_09_16_210646_create_comics_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 250);
+            $table->text('description', 250)->nullable();
+            $table->string('slug', 50)->unique();
+            $table->string('thumb', 250);
+            $table->string('price', 250);
+            $table->string('series', 250);
+            $table->date('sale_date');
+            $table->string('type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comics');
+    }
+};
